de0b298e6ecd090577892adf90ee98cd01edbee7
Nurjahan <nurjahan0787@gmail.com>
2024-11-26 15:18:30 +0600

    version 3

--- Problem1.java
+++ Problem1.java
@@ -16,6 +16,8 @@ public class Problem1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         System.out.println("Hello hi!");
+          System.out.println("Hello ");
     }
     
 }
